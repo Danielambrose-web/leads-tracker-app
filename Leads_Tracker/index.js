7aa104e6abaf1dae3766deeddb3d001c2c72a538
Danielambrose-web <your-email@example.com>
2025-10-18 07:33:11 +0100

    saveleads too local storage and also retrive it for display. and i added a delete button that clears the local storage

--- Leads_Tracker/index.js
+++ Leads_Tracker/index.js
@@ -1,40 +1,45 @@
 let saveEl = document.getElementById("save-btn")
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
-
+let deleteEl = document.getElementById("delete-btn")
 let myleads = []
 
-// let listItems = ""
-function renderLeads(){
+let OlderLeads = JSON.parse(localStorage.getItem("myLeads")) // JSON, not jason
+if (OlderLeads) {
+    myleads = OlderLeads
+    renderLeads()
+}
+
+deleteEl.addEventListener("click", function () {
+    localStorage.clear()
+    myleads = []
+    renderLeads()
+})
+
+console.log(OlderLeads)
+
+function renderLeads() {
     let listItems = ""
-    for(let i = 0; i<myleads.length; i++){
+    for (let i = 0; i < myleads.length; i++) {
         // ulEl.innerHTML += "<li>" + myleads[i] + "</li>"
         listItems += `
-        <li>
-        <a target='_blank' href='${myleads[i]}'>
-        ${myleads[i]}
-        </a>
-        </li>`
+<li>
+  <a target='_blank' href='${myleads[i]}'>
+    ${myleads[i]}
+  </a>
+</li>`
     }
     ulEl.innerHTML = listItems
 }
 
-saveEl.addEventListener("click", function(){
-
+saveEl.addEventListener("click", function () {
     myleads.push(inputEl.value)
     localStorage.setItem("myLeads", JSON.stringify(myleads))
 
     inputEl.value = ""
-
     renderLeads()
-    console.log(localStorage.getItem("myLeads"))
+
     // Get the leads from the localStorage - PS: JSON.parse()
-// Store it in a variable, leadsFromLocalStorage
-// Log out the variable
-
-
-
+    // Store it in a variable, leadsFromLocalStorage
+    // Log out the variable
 })
-
-let OlderLeads =jason.parse( localStorage.getItem("myLeads") )
-console.log(OlderLeads)
